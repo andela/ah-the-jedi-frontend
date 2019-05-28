d87cdce28c9d58f8f0956b4434df57487676c8fc
Alvin Mugambi <ngurual123@gmail.com>
2019-05-28 15:17:39 +0300

    feat(password-reset): implement password reset ensure a user can reset their password [Delivers #165305222]

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import LoginView from '../views/LoginView';
 import Notfound from '../components/layout/Notfound';
 import AccountActivatedView from '../views/AccountActivateView';
 import EmailSentView from '../views/EmailSentView';
+import ResetPassword from '../components/reset_password';
 
 const routes = [
   {
@@ -31,6 +32,15 @@ const routes = [
     path: '/emailsent',
     component: EmailSentView,
     id: 'emailsent',
+  },
+  {
+    path: '/reset_password',
+    component: ResetPassword,
+    id: 'reset_password',
+  },
+  {
+    component: Notfound,
+    id: 'notfound',
   },
   {
     component: Notfound,
