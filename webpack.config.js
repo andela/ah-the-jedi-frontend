63de13bc253883172fbfe03b93a79bbfa40eb1d5
Alvin Mugambi <ngurual123@gmail.com>
2019-05-28 15:17:39 +0300

    feat(password-reset): implement password reset ensure a user can reset their password [Delivers #165305222]

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] }
     ],
   },
   devServer: {
