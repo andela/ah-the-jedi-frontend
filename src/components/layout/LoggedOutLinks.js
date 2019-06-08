294cbbbac53858150534a5b7bc30a6fb97b94634
jkamz <jkamz6432@gmail.com>
2019-06-08 21:29:32 +0300

    bug(styling) fix responsiveness and social login styling - add button UI support for firefox - add color variables to varaibles.scss - fix responsiveness of the login page - fix responsiveness of the signup page - fix responsiveness on the navigation bar [Fixes #166561719]

--- src/components/layout/LoggedOutLinks.js
+++ src/components/layout/LoggedOutLinks.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../../assets/styles/header.scss';
 
 /*
  *@return {jsx} to display in the Header component
@@ -7,14 +8,14 @@ import { NavLink } from 'react-router-dom';
  * logged out
  */
 const LoggedOutLinks = () => (
-  <>
+  <div className="custom-nav-lo">
     <NavLink className="nav-link" to="/signup">
       Sign up
     </NavLink>
     <NavLink className="nav-link" to="/login">
       Login
     </NavLink>
-  </>
+  </div>
 );
 
 export default LoggedOutLinks;
